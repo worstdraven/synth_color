4ae3ab0201fb4a83f07d1cb87b0d03119ab20f22
sunpot <kadennolan9@gmail.com>
2025-10-25 15:50:45 +0900

    feat: remove codes for breakcout

--- synth_color/App/src/Game.cpp
+++ synth_color/App/src/Game.cpp
@@ -3,13 +3,6 @@
 Game::Game(const InitData& init)
 	: IScene{ init }
 {
-	for (int32 y = 0; y < 5; ++y)
-	{
-		for (int32 x = 0; x < (800 / BrickSize.x); ++x)
-		{
-			m_bricks << Rect{ (x * BrickSize.x), (60 + y * BrickSize.y), BrickSize };
-		}
-	}
 	m_pieces << std::make_unique<TrianglePiece>(Vec2{ 100, 500 }, Vec2{ 200, 600 }, Vec2{ 100, 600 }, ColorF{ 1.0, 1.0, 0.0, 0.2 });
 	m_pieces << std::make_unique<TrianglePiece>(Vec2{ 300, 500 }, Vec2{ 400, 600 }, Vec2{ 300, 600 }, ColorF{ 0.0, 1.0, 1.0, 0.2 });
 	m_pieces << std::make_unique<TrianglePiece>(Vec2{ 500, 500 }, Vec2{ 600, 600 }, Vec2{ 500, 600 }, ColorF{ 1.0, 0.0, 1.0, 0.2 });
@@ -36,17 +29,9 @@ void Game::draw() const
 {
 	Scene::SetBackground(ColorF{ 0.2 });
 
-	// スコアを描く
-	FontAsset(U"Bold")(m_score).draw(24, Vec2{ 400, 16 });
-
 	for (const auto& poly : m_pieces) {
 		poly->draw();
 	}
-}
-
-Rect Game::getPaddle() const
-{
-	return{ Arg::center(Cursor::Pos().x, 500), 60, 10 };
 }
 
 void BasePiece::update() {
--- synth_color/App/src/Game.hpp
+++ synth_color/App/src/Game.hpp
@@ -45,29 +45,6 @@ public:
 
 private:
 
-	// ブロックのサイズ
-	static constexpr Size BrickSize{ 40, 20 };
-
-	// ボールの速さ
-	static constexpr double BallSpeed = 480.0;
-
-	// ボールの速度
-	Vec2 m_ballVelocity{ 0, -BallSpeed };
-
-	// ボール
-	Circle m_ball{ 400, 400, 8 };
-
-	// ブロックの配列
-	Array<Rect> m_bricks;
-
-	// 現在のゲームのスコア
-	int32 m_score = 0;
-
-	// ブロックを壊したときの効果音
-	Audio m_brickSound{ GMInstrument::Woodblock, PianoKey::C5, 0.2s, 0.1s };
-
-	Rect getPaddle() const;
-
 	Array<std::unique_ptr<BasePiece>> m_pieces;
 };
 
