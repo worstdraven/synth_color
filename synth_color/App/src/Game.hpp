47fb6d8e00e4af0f7f16f13a5c8157b36cce1b77
sunpot <kadennolan9@gmail.com>
2025-10-28 02:04:30 +0900

    feat: add sticky grid

--- synth_color/App/src/Game.hpp
+++ synth_color/App/src/Game.hpp
@@ -11,6 +11,7 @@ public:
 
 	virtual ~BasePiece() = default;
 
+
 	inline void draw() const {
 		m_polygon.draw(m_color);
 	}
@@ -18,6 +19,11 @@ public:
 	inline Polygon getPolygon() const {
 		return m_polygon;
 	}
+
+	inline Vec2 getPrimaryPos() const {
+		return m_polygon.vertices()[0];
+		//return m_primary_pos;
+	};
 
 	inline void moveBy(const Vec2& delta) {
 		m_polygon.moveBy(delta);
@@ -30,6 +36,7 @@ public:
 protected:
 	Color m_color{ 255, 255, 255 };
 	Polygon m_polygon;
+	//Vec2 m_primary_pos;
 };
 
 class TrianglePiece : public BasePiece {
@@ -37,6 +44,7 @@ public:
 	inline TrianglePiece(const Vec2& p1, const Vec2& p2, const Vec2& p3, const Color& color)
 		: BasePiece{ color } {
 		m_polygon = Triangle{ p1, p2, p3 }.asPolygon();
+		//m_primary_pos = p1;
 	};
 };
 
@@ -45,6 +53,7 @@ public:
 	inline RectanglePiece(const Vec2& pos, const Size& size, const Color& color)
 		: BasePiece{ color } {
 		m_polygon = RectF{ pos, size }.asPolygon();
+		//m_primary_pos = pos;
 	};
 };
 
@@ -53,6 +62,7 @@ public:
 	inline CirclePiece(const Vec2& center, double radius, const Color& color)
 		: BasePiece{ color } {
 		m_polygon = Circle{ center, radius }.asPolygon(30);
+		//m_primary_pos = center;
 	};
 };
 
@@ -76,5 +86,7 @@ private:
 	// 図形の配列
 	Array<std::unique_ptr<BasePiece>> m_pieces;
 
+	// グリッドサイズ
+	const double grid_size = 30.0;
 };
 
