83a74c4ba15b3b25e29c745df52c3a0c5e7e1067
sunpot <kadennolan9@gmail.com>
2025-10-29 02:42:17 +0900

    feat: add member to simplify. add range of motion.

--- synth_color/App/src/Game.cpp
+++ synth_color/App/src/Game.cpp
@@ -11,24 +11,24 @@ Game::Game(const InitData& init)
 
 void Game::update()
 {
-	const double w = Scene::Width();
-	const double h = Scene::Height();
-	const Point viewportTopLeft = Vec2{ w * 0.45, h * 0.15 }.asPoint();
-
 	{
-		const Transformer2D t{ Mat3x2::Identity(), Mat3x2::Translate(viewportTopLeft) };
+		const Transformer2D t{ Mat3x2::Identity(), Mat3x2::Translate(m_puzzleViewportTopLeft) };
 
 		// 図形のドラッグ処理
 		if (m_selectedPieceIndex != -1) {
-			// マウスが話されたとき
+			// マウスが離されたときの処理
 			if (MouseL.up()) {
-				Vec2 pos = m_pieces[m_selectedPieceIndex]->getPrimaryPos() + viewportTopLeft;
-				Vec2 dest = { std::ceil(pos.x / m_gridSize - 0.5) * m_gridSize, ::ceil(pos.y / m_gridSize - 0.5) * m_gridSize };
+				Vec2 pos = m_pieces[m_selectedPieceIndex]->getPrimaryPos();
+				// グリッドにスナップ
+				Vec2 dest{ std::ceil(pos.x / m_gridSize - 0.5) * m_gridSize, ::ceil(pos.y / m_gridSize - 0.5) * m_gridSize };
+				// ビューポート外に出ないように制限
+				dest.x = std::min(std::max(20.0, dest.x), static_cast<double>(m_puzzleViewportRect.w - 20.0));
+				dest.y = std::min(std::max(20.0, dest.y), static_cast<double>(m_puzzleViewportRect.h - 20.0));
 				Print << dest << pos;
 				m_pieces[m_selectedPieceIndex]->moveBy(dest - pos);
 				m_selectedPieceIndex = -1;
 			}
-			// ドラッグされている間
+			// ドラッグされている間、位置をマウスに追従させる
 			else {
 				m_pieces[m_selectedPieceIndex]->moveBy(Cursor::DeltaF());
 			}
@@ -53,28 +53,24 @@ void Game::update()
 
 void Game::draw() const
 {
-	Scene::SetBackground(Palette::Steelblue);
+	Scene::SetBackground(Palette::Gray);
 
-	const double w = Scene::Width();
-	const double h = Scene::Height();
-	const Point viewportTopLeft = Vec2{ w * 0.45, h * 0.15 }.asPoint();
-	const Rect viewportRect{ viewportTopLeft, static_cast<int>(w * 0.5), static_cast<int>(h * 0.75) };
-	viewportRect.rounded(10.0).draw(Palette::White).drawFrame(2, Palette::Black);
+	FontAsset(U"Bold")(U"ステージ1").draw(48, Arg::bottomLeft(Scene::Width() * .68, Scene::Height() * .14));
+	m_puzzleViewportRect.rounded(10.0).draw(Palette::White).drawFrame(2, Palette::Black);
 
 	{
 		// パズルビューポート
-		const ScopedViewport2D viewport{ viewportRect };
+		const ScopedViewport2D viewport{ m_puzzleViewportRect };
+		const Transformer2D t{ Mat3x2::Identity(), Mat3x2::Translate(m_puzzleViewportTopLeft) };
 
-		const Transformer2D t{ Mat3x2::Identity(), Mat3x2::Translate(viewportTopLeft) };
-
-		for (int i = m_gridSize; i < viewportRect.w; i += m_gridSize) {
-			for (int j = m_gridSize; j < viewportRect.h; j += m_gridSize) {
+		for (int i = m_gridSize; i < m_puzzleViewportRect.w; i += m_gridSize) {
+			for (int j = m_gridSize; j < m_puzzleViewportRect.h; j += m_gridSize) {
 				Circle{ i, j, 1 }.draw(Palette::Black);
 			}
 		}
 
 		{
-			// 減算ブレンドで描画
+			// 減算ブレンドで全ピースを描画
 			const ScopedRenderStates2D blend{ BlendState::Subtractive };
 
 			for (const auto& poly : m_pieces) {
@@ -95,5 +91,6 @@ void Game::draw() const
 		}
 	}
 
-	RectF{ w * 0.05, h * 0.2, w * 0.35, h * 0.6 }.rounded(10.0).draw(Palette::Black);
+	// 左側のお手本パネル
+	RectF{ Scene::Width() * 0.05, Scene::Height() * 0.2, Scene::Width() * 0.35, Scene::Height() * 0.6 }.rounded(10.0).draw(Palette::Black);
 }
