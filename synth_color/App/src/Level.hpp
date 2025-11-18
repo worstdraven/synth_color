c03be78fc5d4b76380b0b9a968d2426d057b7608
sunpot <kadennolan9@gmail.com>
2025-11-19 02:16:59 +0900

    merge conflict

--- synth_color/App/src/Level.hpp
+++ synth_color/App/src/Level.hpp
@@ -42,5 +42,5 @@ private:
 
 	Effect m_effect;
 
-	Transition m_changeSceneTransition{ TitleToLevelDuration / 2.0, LevelToGameDuration / 2.0 }; 
+	Transition m_changeSceneTransition{ TitleToLevelDuration / 2.0, DisappearDuration };
 };
